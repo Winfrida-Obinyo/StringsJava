d3b807b4591300106c98f11eb358941a7e0d35ed
Winfrida-Obinyo <obinyowinfridaa@gmail.com>
2023-03-13 15:12:37 +0300

    StringsJava

--- strings.js
+++ strings.js
@@ -1,11 +1,36 @@
-let b = "beautiful";
 
-console.log(b[5],b[6],b[7],b[8]);
-
-String.prototype.insert = function(index, string){
-    if (index > 0)
-    {
-        return food.substring(0,index) + string + food.substring(index, food.length);
-    }
-    return string + food
-}
+let Winfrida = "beautiful";
+console.log(Winfrida.length);
+console.log(Winfrida[8]);
+console.log(Winfrida[7]);
+console.log(Winfrida[6]);
+console.log(Winfrida[5]);
+const food = "I was feeling hungry today";
+const mystring = "eat";
+console.log(food[4]);
+const result = `I was ${newstring} feeling hungry today`;
+console.log(result);
+"the"
+"brown"
+const story = "The quick brown fox jumps over the lazy dog";
+console.log(story.match(/the/g).length);
+console.log(story.match(/brown/g).length);
+const string1 = "We are now going to school";
+console.log(string1.includes("now"));
+console.log(string5.includes("sitting"));
+const string2 = ("The child was sitting on the table before it fell")
+console.log(string2.includes("sitting"));
+//convert
+//1. to Uppercase
+let wonder
+= "wonderful"
+console.log(v.toUpperCase())
+//2 . to lowercase
+let amaz= "amazing"
+let neath = "BEneath"
+console.log(amaz.toLocaleLowerCase())
+console.log(neath.toLocaleLowerCase())
+//3.TITleCASE
+let wedd="A beautiful wedding";
+    wedd=wedd.split(" ").map(([firstChar,...rest])=>firstChar.toUpperCase()+rest.join("").toLowerCase()).join(" ");
+       console.log(wedd);
